1fe1e58aab365720e8bd8a5f7ee041c584f3f0a4
Eugene-Barcode <97022343+Eugene-Barcode@users.noreply.github.com>
2022-05-18 15:28:22 +0300

    Added name and nickname

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello, git");
-        System.out.println("The second commit")
+        System.out.println("The second commit");
+        String name = "Yevhen";
+        String nickname = "Barcode";
     }
 }
