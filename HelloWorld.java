3b8078ab5505de20c5050497a6adfb10d4fa8ee0
Yevhen Zavelya <koshanua@gmail.com>
2022-05-18 15:52:58 +0300

    Added some lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,12 +4,14 @@ public class HelloWorld {
         System.out.println("The second commit");
         String name = "Eugene";
         String nickname = "Barcode";
-        System.out.println(name + nickname);
+        System.out.println(name + " " + nickname);
+        System.out.println("Let the countdown begin");
+        Counter.main();
     }
 }
 
 class Counter {
-    public static void main (String[] args) {
+    public static void main () {
         int x = 10;
         while (x != 0) {
             System.out.println("Counting " + x);
